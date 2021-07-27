595146c85032e39ebf405572e910ee440627aa38
Danil <danil.mirtalibov.27@mail.ru>
2021-07-28 00:18:53 +0300

    Started the get_file route

--- mediaserver/core/routes/file_uploder.py
+++ mediaserver/core/routes/file_uploder.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import JSONResponse
 import uuid
 import shutil
+import os
 
 from loguru import logger
 
@@ -14,7 +15,7 @@ async def load_file(
     type: str,
     file_data: UploadFile = File(...),
 ):
-    """Sends video to the mediaserver and returns it's id/url on mediaserver"""
+    """ Saves file to a correct folder and returnes it's id """
 
     file_id = str(uuid.uuid4().hex)
 
@@ -31,3 +32,22 @@ async def load_file(
         shutil.copyfileobj(file_data.file, buffer)
 
     return {"filename": file_id}
+
+
+@router.get("/get_file", status_code=200, responses={404: {"model": str}})
+async def get_file(type: str, file_id: str):
+    """ Searches file with specified id in the specified folder and returns it """
+
+    if type == "video":
+        file_path = f"core/videos/{file_id}.mp4"
+        dir = "videos"
+    elif type == "image":
+        file_path = f"core/images/{file_id}.jpg"
+        dir = "images"
+    else:
+        message = "Wrong type passed"
+        logger.warning(message)
+        return JSONResponse(status_code=409, content={"message": message})
+
+    files_in_dir = os.listdir(f"core/{dir}/")
+    logger.info(files_in_dir)
