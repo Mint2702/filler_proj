f04b92d7c012608a4b511800a50095b32fbfee1a
Danil <danil.mirtalibov.27@mail.ru>
2021-08-03 16:42:57 +0300

    path fixes

--- mediaserver/core/storages.py
+++ mediaserver/core/storages.py
@@ -7,7 +7,7 @@ class LocalStorage:
     @staticmethod
     async def save(file_name: str, second_dir: str, data: bytes, offset: int) -> None:
         path = str(Path(".").absolute())
-        async with AIOFile(f"{path}/{second_dir}/{file_name}", mode="ab") as file:
+        async with AIOFile(f"{path}/{second_dir}{file_name}", mode="ab") as file:
             await file.write(data, offset=offset)
             await file.fsync()
             logger.info(f"Writing in the {file_name}")
