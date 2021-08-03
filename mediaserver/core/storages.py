33153e258a4e1ceb3d8aa74fa7c1e08749bfa9ca
Danil <danil.mirtalibov.27@mail.ru>
2021-08-03 18:58:25 +0300

    Path changes

--- mediaserver/core/storages.py
+++ mediaserver/core/storages.py
@@ -1,7 +1,6 @@
 from aiofile import AIOFile, async_open
 from loguru import logger
 from pathlib import Path
-import os
 
 from .settings import settings
 
@@ -9,17 +8,12 @@ from .settings import settings
 class LocalStorage:
     @staticmethod
     async def save(file_name: str, second_dir: str, data: bytes, offset: int) -> None:
-        path = settings.mediaserver_path
-        mediaserver_path = str(Path(".").absolute())
-        print(mediaserver_path)
-        home = str(Path.home())
-        files = [f for f in os.listdir(home)]
-        for f in files:
-            print(f)
-        async with AIOFile(f"{home}{file_name}", mode="ab") as file:
+        path = str(Path(".").absolute())
+        async with AIOFile(f"{path}/{second_dir}{file_name}", mode="ab") as file:
             await file.write(data, offset=offset)
             await file.fsync()
             logger.info(f"Writing in the {file_name}")
+
 
     @staticmethod
     async def get(file_name: str, second_dir: str, start: int, end: int) -> bytes:
